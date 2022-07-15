74d86dd5fd230568ca4791752f100562f98512c5
Qasim Abdullah <qasimabdullah404@gmail.com>
2022-07-15 20:15:33 +0500

    implementation

---
+++ lib/tipsty/builder.rb
@@ -0,0 +1,36 @@
+class String
+  def integer? 
+    return true if self =~ /^[1-9]\d*(\.\d+)?$/
+    false
+  end
+end
+
+module Tipsty
+	class Builder
+    def initialize total:, gratuity:
+      @total = total
+      @gratuity = gratuity
+    end
+
+    def generate
+      return calculation if number_based?
+      string_based
+    end
+
+    def number_based?
+      (@gratuity.is_a? Numeric) || (@gratuity.integer?)
+    end
+
+    def string_based
+      case @gratuity.downcase
+      when 'high'     then calculation 25
+      when 'standard' then calculation 18
+      when 'low'      then calculation 15
+      end
+    end
+
+    def calculation gratuity = @gratuity
+      @total += @total * (gratuity.to_f / 100)
+    end
+  end
+end
